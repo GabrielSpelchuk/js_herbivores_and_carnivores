c010018cbb77373daabe1c8547782cc34033e7c9
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2026-02-16 11:56:53 +0200

    replaced in a remove dead animal method !== to >

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -32,7 +32,7 @@ class Carnivore extends Animal {
       animal.health -= 50;
 
       if (animal.health <= 0) {
-        Animal.alive = Animal.alive.filter((animals) => animals.health !== 0);
+        Animal.alive = Animal.alive.filter((animals) => animals.health > 0);
       }
     }
   }
